4ad1c475b6a42cd826906f156f2c1db759f04a24
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-02-07 23:45:38 +0300

    end

---
+++ main.js
@@ -0,0 +1,15 @@
+function clickedButton1(){
+    document.getElementById("container").style.backgroundImage = "url('wallpaper1.jpg')";
+}
+
+function clickedButton2(){
+    document.getElementById("container").style.backgroundImage = "url('wallpaper2.jpg')";
+}
+
+function clickedButton3(){
+    document.getElementById("container").style.backgroundImage = "url('wallpaper3.jpg')";
+}
+
+function clickedButton4(){
+    document.getElementById("container").style.backgroundImage = "url('wallpaper4.jpg')";
+}
